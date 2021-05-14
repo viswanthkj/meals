8e3e62dd355f9c0826d5e06cebe025d1dd111550
viswanthkj <viswanth1995@gmail.com>
2021-05-14 21:12:38 +0530

    drawer navigation added

--- src/navigation/MealsNavigator.js
+++ src/navigation/MealsNavigator.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-// import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
 import FavoritesScreen from '../screens/FavoritesScreen';
+import FiltersScreen from '../screens/FiltersScreen';
 import Colors from '../constants/Colors';
 
 const defaultStackNavOptions = {
@@ -56,7 +57,8 @@ const tabScreenConfig = {
           <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
         );
       },
-      tabBarColor: Colors.primary
+      tabBarColor: Colors.primary,
+      tabBarLabel: 'Meals'
     }
   },
   Favorites: {
@@ -65,7 +67,8 @@ const tabScreenConfig = {
       tabBarIcon: tabInfo => {
         return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
       },
-      tabBarColor: Colors.accent
+      tabBarColor: Colors.accent,
+      tabBarLabel: 'Favorites'
     }
   }
 };
@@ -85,4 +88,33 @@ const MealsFavTabNavigator =
         }
       });
 
-export default createAppContainer(MealsFavTabNavigator)
+      const FiltersNavigator = createStackNavigator(
+        {
+          Filters: FiltersScreen
+        },
+        {
+          // navigationOptions: {
+          //   drawerLabel: 'Filters!!!!'
+          // },
+          defaultNavigationOptions: defaultStackNavOptions
+        }
+      );
+      
+      const MainNavigator = createDrawerNavigator(
+        {
+          MealsFavs: {
+            screen: MealsFavTabNavigator,
+            navigationOptions: {
+              drawerLabel: 'Meals'
+            }
+          },
+          Filters: FiltersNavigator
+        },
+        {
+          contentOptions: {
+            activeTintColor: Colors.accentColor,
+          }
+        }
+      );
+
+export default createAppContainer(MainNavigator)
--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { StyleSheet,FlatList } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton';
+
 import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
 
@@ -37,9 +41,22 @@ const CategoriesScreen = props => {
 
 export default CategoriesScreen;
 
-CategoriesScreen.navigationOptions = {
-    headerTitle: 'Meal Categories'
-}
+CategoriesScreen.navigationOptions = navData => {
+  return {
+    headerTitle: 'Meal Categories',
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
+};
 
 
 const styles = StyleSheet.create({
